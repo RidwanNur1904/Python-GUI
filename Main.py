c16a80024e07761b743134568a59bd3a3fe39032
Blizztewa <91624383+Blizztewa@users.noreply.github.com>
2023-11-16 14:28:58 +0000

    nothing

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import tkinter
 import customtkinter
-from pytube import YouTube
+#from pytube import YouTube
 
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-,blue green
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
 app.geometry("720x480")
